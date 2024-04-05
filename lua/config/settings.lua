f39c1f6451631d14d5d6838654f802f29db1a100
toumeh <dev.toumeh@gmail.com>
2024-04-06 01:56:01 +0200

    Add markdown and PHP languages to lsp
    - add spell functionality
    - add more key maps

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,10 +1,79 @@
+local map = vim.keymap.set
+
 vim.cmd([[
 set expandtab
 set tabstop=2
 set softtabstop=2
 set shiftwidth=2
+set spell
+set spelllang=en_us
+
 ]])
 vim.g.mapleader = " "
 vim.opt.number = true -- enable line numbers
 vim.wo.relativenumber = true
-vim.keymap.set('n', '<Ctrl>#', ':b#', {noremap = true, silent = true})
+
+-- Remap for dealing with visual line wraps
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+
+-- better indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- paste over currently selected text without yanking it
+map("v", "p", '"_dp')
+map("v", "P", '"_dP')
+
+-- window
+map("n", "<leader>ws", "<cmd>split<cr>", { desc = "Horizontal split" })
+map("n", "<leader>wv", "<cmd>vsplit<cr>", { desc = "Vertical split" })
+map("n", "<leader>wc", "<cmd>close<cr>", { desc = "Close" })
+map("n", "<leader>wm", "<cmd>WindowsMaximize<cr>", { desc = "Maximize" })
+map("n", "<leader>wr", "<cmd>wincmd r<cr>", { desc = "rotate down/right" })
+map("n", "<leader>wR", "<cmd>wincmd R<cr>", { desc = "rotate up/left" })
+map("n", "<leader>wH", "<cmd>wincmd H<cr>", { desc = "Move left" })
+map("n", "<leader>wJ", "<cmd>wincmd J<cr>", { desc = "Move down" })
+map("n", "<leader>wK", "<cmd>wincmd K<cr>", { desc = "Move up" })
+map("n", "<leader>wL", "<cmd>wincmd L<cr>", { desc = "Move right" })
+map("n", "<leader>w=", "<cmd>wincmd =<cr>", { desc = "Equalize size" })
+map("n", "<leader>wk", "<cmd>resize +5<cr>", { desc = "Up" })
+map("n", "<leader>wj", "<cmd>resize -5<cr>", { desc = "Down" })
+map("n", "<leader>wh", "<cmd>vertical resize +3<cr>", { desc = "Left" })
+map("n", "<leader>wl", "<cmd>vertical resizce -3<cr>", { desc = "Right" })
+
+-- buffers
+map("n", "<leader>bD", "<cmd>%bd|e#|bd#<cr>", { desc = "Close all but the current buffer" })
+
+-- check this when you gain more experience
+-- move over a closing element in insert mode
+-- map("i", "<C-l>", function()
+--  return require("utils.functions").escapePair()
+--end)
+
+
+-- save like your are used to
+map({ "i", "v", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+
+-- new file
+map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New file" })
+-- save file
+map("n", "<leader>fs", "<cmd>w<cr>", { desc = "Save file" })
+
+-- Spelling
+map("n", "<leader>zl", "<cmd>Telescope spell_suggest<cr>", { desc = "List corrections" })
+map("n", "<leader>zf", "1z=", { desc = "Use first correction" })
+map("n", "<leader>zj", "]s", { desc = "Next error" })
+map("n", "<leader>zk", "[s", { desc = "Previous error" })
+map("n", "<leader>za", "zg", { desc = "Add word" })
+
+
+
+
+
+
+
+
+
+
+
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -6,11 +6,30 @@ return
         local lspconfig = require("lspconfig")
         lspconfig.lua_ls.setup({})
         lspconfig.gopls.setup{}
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-      end
-
+        lspconfig.phpactor.setup{}
+        lspconfig.yamlls.setup{
+          cmd = {"yaml-language-server", "--stdio"},
+          filetypes = {"yaml", "yaml.docker-compose"},
+          root_dir = lspconfig.util.find_git_ancestor,
+          settings = {
+            yaml = {
+              schemas = {
+                ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = 
+                  "/home/naseem91/PhpstormProjects/php-dockerCompose-devstack",
+              },
+            },
+            redhat = {
+              telemetry = {
+                enabled = false,
+              },
+            },
+          },
+          single_file_support = true,
+        }
+             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+          end
    },
   {
     "williamboman/mason.nvim",
@@ -22,7 +41,7 @@ return
    "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup {
-      ensure_installed = { "lua_ls", "gopls"},
+      ensure_installed = { "lua_ls", "gopls", "phpactor", "yamlls"},
       }
     end
   }
