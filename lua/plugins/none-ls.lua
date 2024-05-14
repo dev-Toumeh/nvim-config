0ee5f501bc73c8ba18ad99a84e9bbd837d0d00c5
toumeh <dev.toumeh@gmail.com>
2024-05-14 07:15:59 +0200

    add suppot for more languages

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,10 +7,13 @@ return {
 				null_ls.builtins.formatting.stylua, -- lua
 				null_ls.builtins.formatting.gofumpt,
 				null_ls.builtins.formatting.goimports_reviser,
-				null_ls.builtins.formatting.golines,
-				null_ls.builtins.formatting.shfmt
+				null_ls.builtins.formatting.golines, -- go
+				null_ls.builtins.formatting.shfmt, -- sh 
+        null_ls.builtins.formatting.prettier, -- "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css", "scss", "less", "html"
+        null_ls.builtins.diagnostics.djlint   --(keywords: django, go, nunjucks, twig, handlebars, mustache, angular, jinja)
+
 			},
 		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		vim.keymap.set("n", "mf", vim.lsp.buf.format, { desc = "format if there is none-ls source"})
 	end,
 }
