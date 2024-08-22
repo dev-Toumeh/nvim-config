0bc2a00536a15dd33377c5311cb764954f8e0af2
toumeh <dev.toumeh@gmail.com>
2024-08-22 16:02:08 +0200

    reactive neo-tree

--- lua/plugins/new-tree.lua
+++ lua/plugins/new-tree.lua
@@ -1,16 +1,14 @@
-return {}
---
---      "nvim-neo-tree/neo-tree.nvim",
---      branch = "v3.x",
---      dependencies =
---       {
---         "nvim-lua/plenary.nvim",
---         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
---         "MunifTanjim/nui.nvim",
---       },
---        config = function()
---          -- neo-tree keymap
---         vim.keymap.set('n', '<C-n>',"<cmd>:Neotree filesystem reveal left<CR>")
---         vim.keymap.set('n', '<C-t>',"<cmd>:Neotree toggle<CR>")
---        end
---}
+return {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies =
+       {
+         "nvim-lua/plenary.nvim",
+         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+         "MunifTanjim/nui.nvim",
+       },
+        config = function()
+         vim.keymap.set('n', '<C-n>',"<cmd>:Neotree filesystem reveal left<CR>")
+         vim.keymap.set('n', '<C-t>',"<cmd>:Neotree toggle<CR>")
+        end
+}
