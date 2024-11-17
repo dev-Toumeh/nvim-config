442a63f39639bce50b46fc3f244705746597dc08
Naseem Toumeh <dev.toumeh@gmail.com>
2024-11-17 08:19:25 +0100

    add better desc for gitsigns keymapping

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -68,32 +68,18 @@ return {
 					end)
 
 					-- Actions
-					vim.keymap.set(
-						"n",
-						"<leader>gp",
-						":Gitsigns preview_hunk<CR>",
-						{ desc = "preview change in the code" }
-					) --
-					map("n", "<leader>gd", gitsigns.diffthis) --
-					map("n", "<leader>gr", gitsigns.reset_hunk) --
-					map("n", "<leader>gh", gitsigns.toggle_current_line_blame)
-					map("v", "<leader>hs", function()
-						gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-					end)
-					map("n", "<leader>hs", gitsigns.stage_hunk)
-					map("v", "<leader>hr", function()
-						gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-					end)
-					map("n", "<leader>hS", gitsigns.stage_buffer)
-					map("n", "<leader>hu", gitsigns.undo_stage_hunk)
-					map("n", "<leader>hR", gitsigns.reset_buffer)
-					map("n", "<leader>hp", gitsigns.preview_hunk)
-					map("n", "<leader>hb", function()
-						gitsigns.blame_line({ full = true })
-					end)
-					map("n", "<leader>hD", function()
-						gitsigns.diffthis("~")
-					end)
+					map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "reset the current hunk" })
+					map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "stage the current hunk" })
+					map("v", "<leader>hs", function() gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, { desc = "stage the current hunk virtual mode" })
+          map("v", "<leader>hr", function() gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, { desc = "reset the current hunk virtual mode" })
+					map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "stage the current file" })
+					map("n", "<leader>hu", gitsigns.undo_stage_hunk, { desc = "stage the current file" })
+					map( "n", "<leader>hR", gitsigns.reset_buffer, { desc = "reset all the unstaged changes in the current file" })
+          map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "preview the hunk" })
+					map("n", "<leader>hb", function() gitsigns.blame_line({ full = true }) end)
+					map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+					map("n", "<leader>hd", gitsigns.diffthis, { desc = "diff the current stage and the previews commit" })
+					map("n", "<leader>hD", function() gitsigns.diffthis("~") end)
 					map("n", "<leader>td", gitsigns.toggle_deleted)
 
 					-- Text object
@@ -103,6 +89,6 @@ return {
 		end,
 	},
 	{
-  "tpope/vim-fugitive"
-  },
+		"tpope/vim-fugitive",
+	},
 }
